495b828798d765b58f728456749945f1d9cc97f3
mainStorne <dbevzpr@mail.ru>
2025-05-24 13:01:34 +0000

    fix

--- create-all.sh
+++ create-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker service create --name registry --publish published=5000,target=5000 registry:2
-docker compose -f docker-compose.prod.yaml build agent
-docker compose -f docker-compose.prod.yaml push agent
+docker compose -f docker-compose.prod.yaml build agent web
+docker compose -f docker-compose.prod.yaml push agent web
 docker stack deploy -c docker-compose.prod.yaml thesis
